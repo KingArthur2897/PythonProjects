8a73f5afcd4ac501c07b18644f90928e4e1388c2
Artur Baniuk <49306109+KingArthur2897@users.noreply.github.com>
2023-07-19 13:29:16 +0300

    Task5
    Проверка закона Де Моргана с измерением времени, затраченного на решение и набором предикатов от 3 до 15

--- Seminar2.py
+++ Seminar2.py
@@ -1,7 +1,23 @@
-from decimal import Decimal
+import random
+import time
 
-num = Decimal(input("Input number:\n"))
+def check_statement(predicates):
+    check1 = predicates[0]
+    check2 = not predicates[0]
+    for i in range(len(predicates)):
+        check1 = check1 or predicates[i]
+        check2 = check2 and not predicates[i]
+    return not check1 == check2
 
-digits = num.as_tuple().digits
+def check_all(i):
 
-print(sum(digits))
+    count_predicates = random.randint(3,15)
+    predicates = []
+    for i in range(count_predicates):
+        predicates.append(bool(random.randint(0,1)))
+    print(f"Predicates: {predicates} -> {check_statement(predicates)}\n")
+
+start_time = time.time()
+for i in range(100):
+    check_all(i)
+print(f"time: {time.time() - start_time}")
