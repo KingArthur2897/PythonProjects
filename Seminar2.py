e90062f23c1d63d2c1ca4d8528b8fba6ae7b4300
Artur Baniuk <49306109+KingArthur2897@users.noreply.github.com>
2023-07-19 14:38:11 +0300

    Task6
    Нахождение всех делителей числа

--- Seminar2.py
+++ Seminar2.py
@@ -1,23 +1,14 @@
-import random
-import time
+from math import sqrt
 
-def check_statement(predicates):
-    check1 = predicates[0]
-    check2 = not predicates[0]
-    for i in range(len(predicates)):
-        check1 = check1 or predicates[i]
-        check2 = check2 and not predicates[i]
-    return not check1 == check2
-
-def check_all(i):
-
-    count_predicates = random.randint(3,15)
-    predicates = []
-    for i in range(count_predicates):
-        predicates.append(bool(random.randint(0,1)))
-    print(f"Predicates: {predicates} -> {check_statement(predicates)}\n")
-
-start_time = time.time()
-for i in range(100):
-    check_all(i)
-print(f"time: {time.time() - start_time}")
+def all_divisors(n):
+    divisors = []
+    for i in range(1, round(sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    divisors.sort()
+    return divisors
+numbers = [23436, 190187200, 380457890232]
+for i in range(len(numbers)):
+    print(f"Divisors of {numbers[i]}:\n {all_divisors(numbers[i])}")
