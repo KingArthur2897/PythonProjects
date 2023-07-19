4e1d2f7b997154cf926b3d929d3b0105e2e3fce8
Artur Baniuk <49306109+KingArthur2897@users.noreply.github.com>
2023-07-19 12:41:18 +0300

    Task3

--- Seminar2.py
+++ Seminar2.py
@@ -1,14 +1,9 @@
-from math import sqrt
+n = int(input("Input limit of powers of two\n"))
 
-def calc(sum, mul):
-    d = sum * sum + 4 * mul
-    if d > 0:
-        sq = sqrt(d)/2
-        p = sum/2
-        x1 = p - sq
-        x2 = p + sq
-        return [x1,x2]
+def powers(limit):
+    t = 2
+    while t <= limit:
+        print(t)
+        t*=2
 
-S = int(input("Input sum:\n"))
-P = -int(input("Input mul:\n"))
-print(calc(S,P))
+powers(n)
